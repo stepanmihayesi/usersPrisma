5b83a17efe643470c8a6e68a9c87c6255b24a1be
Stepan Mihayesi <smihayesi@gmail.com>
2024-06-20 22:33:10 +0200

    feat: ValidationPipe added globally to the app in order to allow input checks like IsEmail, IsNotEmpty, IsString, MinLength etc.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 if(!process.env.IS_TS_NODE){
   require('module-alias/register');
 }
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
