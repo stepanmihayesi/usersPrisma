5cfb10e3265e4ce735e678e97894860a568783b8
Stepan Mihayesi <smihayesi@gmail.com>
2024-06-22 03:05:49 +0200

    chore: Methods rearranged in logical order.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -13,18 +13,14 @@ export class AuthService {
     private jwtService: JwtService,
     private config: ConfigService,
   ) {}
-  async logout(userId: number) {
-    await this.prisma.user.updateMany({
-      where: {
-        id: userId,
-        hashedRt: {
-          not: '',
-        },
-      },
-      data: {
-        hashedRt: '',
-      },
+  async createAccount(dto: AuthDto): Promise<Tokens> {
+    const hash = await argon2.hash(dto.mdp);
+    const newUser = await this.prisma.user.create({
+      data: { email: dto.email, mdp: hash, hash: hash, hashedRt: hash },
     });
+    const tokens = await this.getTokens(newUser.id, newUser.email);
+    await this.updateRtHash(newUser.id, tokens.refresh_token);
+    return tokens;
   }
   async login(dto: AuthDto): Promise<Tokens> {
     const user = await this.prisma.user.findFirst({
@@ -39,48 +35,16 @@ export class AuthService {
     await this.updateRtHash(user.id, tokens.refresh_token);
     return tokens;
   }
-  async createAccount(dto: AuthDto): Promise<Tokens> {
-    const hash = await argon2.hash(dto.mdp);
-    const newUser = await this.prisma.user.create({
-      data: { email: dto.email, mdp: hash, hash: hash, hashedRt: hash },
-    });
-    const tokens = await this.getTokens(newUser.id, newUser.email);
-    await this.updateRtHash(newUser.id, tokens.refresh_token);
-    return tokens;
-  }
-  hashData(data: string) {
-    return argon2.hash(data);
-  }
-  async getTokens(userId: number, email: string): Promise<Tokens> {
-    const jwtPayload: JwtPayload = {
-      sub: userId,
-      email: email,
-    };
-
-    const [at, rt] = await Promise.all([
-      this.jwtService.signAsync(jwtPayload, {
-        secret: this.config.get<string>('AT_SECRET'),
-        expiresIn: '15m',
-      }),
-      this.jwtService.signAsync(jwtPayload, {
-        secret: this.config.get<string>('RT_SECRET'),
-        expiresIn: '7d',
-      }),
-    ]);
-
-    return {
-      access_token: at,
-      refresh_token: rt,
-    };
-  }
-  async updateRtHash(userId: number, rt: string): Promise<void> {
-    const hash = await argon2.hash(rt);
-    await this.prisma.user.update({
+  async logout(userId: number) {
+    await this.prisma.user.updateMany({
       where: {
         id: userId,
+        hashedRt: {
+          not: '',
+        },
       },
       data: {
-        hashedRt: hash,
+        hashedRt: '',
       },
     });
   }
@@ -96,5 +60,39 @@ export class AuthService {
     const tokens = await this.getTokens(user.id, user.email);
     await this.updateRtHash(user.id, tokens.refresh_token);
     return tokens;
+  }
+  hashData(data: string) {
+    return argon2.hash(data);
+  }
+  async getTokens(userId: number, email: string): Promise<Tokens> {
+    const jwtPayload: JwtPayload = {
+      sub: userId,
+      email: email,
+    };
+    const [at, rt] = await Promise.all([
+      this.jwtService.signAsync(jwtPayload, {
+        secret: this.config.get<string>('AT_SECRET'),
+        expiresIn: '15m',
+      }),
+      this.jwtService.signAsync(jwtPayload, {
+        secret: this.config.get<string>('RT_SECRET'),
+        expiresIn: '7d',
+      }),
+    ]);
+    return {
+      access_token: at,
+      refresh_token: rt,
+    };
+  }
+  async updateRtHash(userId: number, rt: string): Promise<void> {
+    const hash = await argon2.hash(rt);
+    await this.prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        hashedRt: hash,
+      },
+    });
   }
 }
