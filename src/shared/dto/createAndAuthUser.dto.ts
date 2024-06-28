6ddae4a674e10d0fa726d92f19ecdef4354f2b0a
Stepan Mihayesi <smihayesi@gmail.com>
2024-06-28 20:17:29 +0200

    feat: Validation messages with custom text added.

--- src/shared/dto/createAndAuthUser.dto.ts
+++ src/shared/dto/createAndAuthUser.dto.ts
@@ -1,12 +1,12 @@
 import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CreateAndAuthUserDto {
-  @IsNotEmpty()
-  @IsEmail()
+  @IsNotEmpty({ message: "L'E-mail est un champ obligatoire." })
+  @IsEmail({}, { message: "L'E-mail doit être un e-mail valide." })
   email: string;
-  @IsNotEmpty()
-  @IsString()
-  @MinLength(10, {message: 'Le mot de passe doit contenir au moins dix caractères.'})
+  @IsNotEmpty({ message: "Mot de passe est un champ obligatoire." })
+  @IsString({ message: "Le mot de passe doit être une chaîne de caractères alphanumériques." } )
+  @MinLength(10, {message: "Le mot de passe doit contenir au moins dix caractères."})
   mdp: string;
   name?: string;
   adrPost?: string;
--- src/shared/dto/updateUser.dto.ts
+++ src/shared/dto/updateUser.dto.ts
@@ -1,11 +1,14 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateAndAuthUserDto } from './createAndAuthUser.dto';
-import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class UpdateUserDto extends PartialType(CreateAndAuthUserDto) {
+  @IsEmail({}, { message: "L'E-mail doit être un e-mail valide." })
   email?: string;
+  @IsString({ message: "Le mot de passe doit être une chaîne de caractères alphanumériques." } )
+  @MinLength(10, {message: "Le mot de passe doit contenir au moins dix caractères."})
+  mdp?: string;
   name?: string;
   adrPost?: string;
   comment?: string;
-  mdp?: string;
 }
